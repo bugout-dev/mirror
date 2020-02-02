77310a50f09739b25c073859cc5e96438e087d82
Neeraj Kashyap <nkashy1@gmail.com>
2020-02-01 23:58:45 -0800

    Added setup.py for installation
    This is preamble to creation of Dockerfile and shnorky component
    definition.

--- mirror/cli.py
+++ mirror/cli.py
@@ -1,15 +1,20 @@
 import argparse
+from typing import Callable, Dict
 
 from .populate import populate_cli
 from .github import command as github
 
-SUBCOMMAND_POPULATORS = {
+SUBCOMMAND_POPULATORS: Dict[str, Callable[[argparse.ArgumentParser], None]] = {
     github.subcommand: github.populator,
 }
 
-if __name__ == '__main__':
+def main() -> None:
     parser = argparse.ArgumentParser('mirror - Tools for software project analysis')
     populate_cli(parser, SUBCOMMAND_POPULATORS)
 
     args = parser.parse_args()
     args.func(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+VERSION = '0.1.0'
+
+setup(
+    name='dungineers-mirror',
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    description='mirror: Data analysis for software projects',
+    author='Neeraj Kashyap',
+    author_email='neeraj@simiotics.com',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+    ],
+    url='https://github.com/simiotics/mirror',
+    entry_points={
+        'console_scripts': [
+            'mirror = mirror.cli:main'
+        ]
+    }
+)
