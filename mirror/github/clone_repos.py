2942e331b5ca29646018d5047253f4b0f79a8754
Andrey <andrey@simiotics.com>
2021-01-15 17:13:35 +0300

    Fix bug with path resolver

--- mirror/github/clone_repos.py
+++ mirror/github/clone_repos.py
@@ -12,10 +12,12 @@ from pathlib import Path
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('--output_directory', '-d', default='.', help='Output dir for clone repos.', show_default=True)
 @click.option('--stars-expression', '-s', default='>500', help='Stars search condition. ">200" / "=400" / "<300" as example.', show_default=True)
-@click.option('--languages', '-ls', default='[python]', help='--languages "[o11, o12, o13]" or [o11,o12,o13]', show_default=True)
+@click.option('--languages', '-ls', nargs=0, required=True)
+#@click.option('--users', nargs=0, required=True)
+@click.argument('languages', nargs=-1)
 @click.option('--token', '-t', help='Access token for increase rate limit. Read from $github_token if specify.', default='', show_default=True)
 @click.option('--amount', '-n', help='Amount of repo.', type=int, default=50, show_default=True)
-def clone_repos(output_directory: str, stars_condition: str, languages: Tuple, token: str, amount: int):
+def clone_repos(output_directory: str, stars_expression: str, languages: Tuple, token: str, amount: int):
     """
     Clone repos from search api to output dir.
     Be careful check of upload size not provide
@@ -56,24 +58,27 @@ def clone_repos(output_directory: str, stars_condition: str, languages: Tuple, t
     with click.progressbar(languages) as bar:        
         for lang in bar:
             try:
-                search_expresion = f'stars:{stars_condition}+language:{lang.capitalize()}'
+                search_expresion = f'stars:{stars_expression}+language:{lang.capitalize()}'
 
                 request_url = f'https://api.github.com/search/repositories?q={search_expresion}&per_page={amount}&page=1'
                 search_responce = requests.get(request_url, headers=headers)
 
                 data = json.loads(search_responce.text)
 
-                if not data["items"]:
+                if not data.get("items"):
                     break
 
-                 
+                
+                print(resolve_path)
 
                 for repo in data["items"]:
                     git_url = repo['git_url']
-                    resolve_path = Path(os.path.join(output_directory, f'/{lang.capitalize()}/{repo["name"]}'))
-                    if not os.path.exists(resolve_path):
-                        os.makedirs(resolve_path)
-                    pygit2.clone_repository(git_url, resolve_path)
+                    print(git_url)
+                    out_path = resolve_path / lang.capitalize() / repo["name"]
+                    print(out_path)
+                    if not os.path.exists(out_path):
+                        os.makedirs(out_path)
+                    pygit2.clone_repository(git_url, out_path)
             except:
                 traceback.print_exc()
 
